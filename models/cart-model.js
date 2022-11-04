08e01213d48c9afcef2d18ab0ba5c69d03a0359d
AlexPrysiazhnyi <94091665+AlexPrysiazhnyi@users.noreply.github.com>
2022-11-04 09:42:33 -0400

    Added more logic to the Cart feature

--- models/cart-model.js
+++ models/cart-model.js
@@ -29,6 +29,27 @@ class Cart {
     this.totalPrice += product.price;
     this.items.push(cartItem);
   }
-}
+
+  updateItem(productId, updatedQuantity) {
+
+    for (let i = 0; i < this.items.length; i++) {
+        const item = this.items[i];
+
+        if (item.product.id === productId && updatedQuantity > 0) {
+            const cartItem = {...item};
+            const quantityChange = updatedQuantity - item.quantity;
+            cartItem.quantity = updatedQuantity;
+            cartItem.totalPrice = updatedQuantity * item.product.price;
+            this.items[i] = cartItem;
+  
+            this.totalQuantity += quantityChange;
+            this.totalPrice += quantityChange * item.product.price;
+            return;
+        } else if (item.product.id === productId && updatedQuantity <= 0) {
+
+        }
+      }
+    }
+  }
 
 module.exports = Cart;
