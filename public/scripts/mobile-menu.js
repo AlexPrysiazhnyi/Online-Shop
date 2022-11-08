664e6685bd842affed966b89ed3a2e51277197d3
AlexPrysiazhnyi <94091665+AlexPrysiazhnyi@users.noreply.github.com>
2022-11-08 12:55:11 -0500

    Added Mobile Menu Animation

--- public/scripts/mobile-menu.js
+++ public/scripts/mobile-menu.js
@@ -2,7 +2,17 @@ const mobileMenuBtn = document.getElementById("mobile-menu-btn");
 const mobileMenuAside = document.getElementById("mobile-menu");
 
 const toggleMenuHandler = () => {
-    mobileMenuAside.classList.toggle("open");
-}
+  if (mobileMenuAside.classList.contains("open")) {
+    mobileMenuAside.classList.remove("sliding");
+    setTimeout(() => {
+      mobileMenuAside.classList.remove("open");
+    }, 250);
+    return;
+  }
+  mobileMenuAside.classList.add("open");
+  setTimeout(() => {
+    mobileMenuAside.classList.add("sliding");
+  }, 10);
+};
 
 mobileMenuBtn.addEventListener("click", toggleMenuHandler);
